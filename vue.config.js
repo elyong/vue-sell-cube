67106dd8d1f8d12b023c0ba2e82930a05c139c03
王健 <1162331666@qq.com>
2019-03-25 17:57:14 +0800

    @0.0.3

--- vue.config.js
+++ vue.config.js
@@ -49,6 +49,6 @@ module.exports = {
     config.resolve.alias
       .set("components", resolve("src/components"))
       .set("common", resolve("src/common"))
-      .set("api", resolve("src/api"))
+      .set("api", resolve("src/api"));
   }
 };
